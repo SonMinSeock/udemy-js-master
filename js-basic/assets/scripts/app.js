f5caec0100498e358f54a619e89b9d5da38a6c41
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-07-11 19:42:24 +0900

    함수 코드 나누기(리팩토링):

--- js-basic/assets/scripts/app.js
+++ js-basic/assets/scripts/app.js
@@ -1,13 +1,15 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+function getUserInput() {
+  return parseInt(usrInput.value);
+}
 function add() {
-  //currentResult = currentResult + parseFloat(userInput.value);
-  // currentResult = currentResult + parseInt(userInput.value);
-  currentResult = currentResult + +userInput.value;
-  console.log(typeof currentResult.toString());
+  const enteredNumber = getUserInput();
+  const calcDescription = `${currentResult} + ${enteredNumber}`;
+  currentResult = currentResult + enteredNumber;
 
-  outputResult(currentResult, "");
+  outputResult(currentResult, calcDescription);
 }
 
 addBtn.addEventListener("click", add);
