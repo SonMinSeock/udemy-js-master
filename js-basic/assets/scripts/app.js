f0e0179b5e8ad267518e34e0980297e645b97ad1
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-07-12 19:20:32 +0900

    나머지 버튼의 연산기능 함수를 만들어보자, 리펙토링

--- js-basic/assets/scripts/app.js
+++ js-basic/assets/scripts/app.js
@@ -1,15 +1,48 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function getUserInput() {
+function getUserNumberInput() {
   return parseInt(usrInput.value);
 }
-function add() {
-  const enteredNumber = getUserInput();
-  const calcDescription = `${currentResult} + ${enteredNumber}`;
-  currentResult = currentResult + enteredNumber;
 
+function createAndWriteOutput(opperator, resultBeforeCalc, calcNumber) {
+  const calcDescription = `${resultBeforeCalc} ${opperator} ${calcNumber}`;
   outputResult(currentResult, calcDescription);
 }
 
+function add() {
+  const enteredNumber = getUserNumberInput();
+  const initalResult = currentResult;
+  currentResult = currentResult + enteredNumber;
+
+  createAndWriteOutput("+", initalResult, enteredNumber);
+}
+
+function subtract() {
+  const enteredNumber = getUserNumberInput();
+  const initalResult = currentResult;
+  currentResult = currentResult - enteredNumber;
+
+  createAndWriteOutput("-", initalResult, enteredNumber);
+}
+
+function multiply() {
+  const enteredNumber = getUserNumberInput();
+  const initalResult = currentResult;
+  currentResult = currentResult * enteredNumber;
+
+  createAndWriteOutput("*", initalResult, enteredNumber);
+}
+
+function divide() {
+  const enteredNumber = getUserNumberInput();
+  const initalResult = currentResult;
+  currentResult = currentResult / enteredNumber;
+
+  createAndWriteOutput("/", initalResult, enteredNumber);
+}
+
 addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
