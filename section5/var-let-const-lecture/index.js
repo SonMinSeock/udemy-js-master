9985535fa34bb7ca26b586b8b72c8255d5211d19
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-11-24 18:20:36 +0900

    Var vs let & const - “블록 범위” 소개

---
+++ section5/var-let-const-lecture/index.js
@@ -0,0 +1,23 @@
+var name = "Son";
+var name = "Kim";
+
+// {
+//   let test = 5;
+//   console.log(test);
+// }
+
+// console.log(test);
+
+if (name === "Kim") {
+  let hobies = ["sports", "games"];
+  console.log(hobies);
+}
+
+function greet() {
+  var age = 25;
+  console.log(name, age, hobies);
+}
+
+console.log(name, hobies);
+
+greet();
