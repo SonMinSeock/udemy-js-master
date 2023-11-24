e8d6014a710640ca4af0b5c467febd7b74ef04e5
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-11-24 18:31:36 +0900

    호이스팅 이해하기

--- section5/var-let-const-lecture/index.js
+++ section5/var-let-const-lecture/index.js
@@ -1,23 +1,28 @@
-var name = "Son";
-var name = "Kim";
+// 1.  var, let, const 스코프 이해하기.
+// var name = "Son";
+// var name = "Kim";
 
-// {
-//   let test = 5;
-//   console.log(test);
+// // {
+// //   let test = 5;
+// //   console.log(test);
+// // }
+
+// // console.log(test);
+
+// if (name === "Kim") {
+//   let hobies = ["sports", "games"];
+//   console.log(hobies);
 // }
 
-// console.log(test);
+// function greet() {
+//   var age = 25;
+//   console.log(name, age, hobies);
+// }
 
-if (name === "Kim") {
-  let hobies = ["sports", "games"];
-  console.log(hobies);
-}
+// console.log(name, hobies);
 
-function greet() {
-  var age = 25;
-  console.log(name, age, hobies);
-}
+// greet();
 
-console.log(name, hobies);
-
-greet();
+// 2. 호이스팅 이해.
+console.log(username);
+var username = "Son";
