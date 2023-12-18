1b20b37bf5e201dfa14e258a3406895168f1906b
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-12-18 15:46:17 +0900

    함수는 객체이다!

--- section6/functions-01-starting-code/app.js
+++ section6/functions-01-starting-code/app.js
@@ -4,13 +4,15 @@ function startGame() {
   console.log("Game is Starting...");
 }
 
-const person = {
-  name: "Son MinSeock",
-  greet: function greet() {
-    console.log("Hello there!");
-  },
-};
+// const person = {
+//   name: "Son MinSeock",
+//   greet: function greet() {
+//     console.log("Hello there!");
+//   },
+// };
 
-person.greet();
+// person.greet();
 
+//console.log(typeof startGame);
+console.dir(startGame);
 startGameBtn.addEventListener("click", startGame);
