df7d20fc7194b3f4135d9184c66d5649ee2217d1
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-12-29 21:03:46 +0900

    프로젝트 작업하기: 게임에 사용자의 선택 추가하기

--- section6/functions-01-starting-code/app.js
+++ section6/functions-01-starting-code/app.js
@@ -1,22 +1,28 @@
 const startGameBtn = document.getElementById("start-game-btn");
 
-const start = function () {
-  console.log("Game is Starting...");
+const ROCK = "ROCK";
+const PAPER = "PAPER";
+const SCISSORS = "SCISSORS";
+const DEFAULT_USER_CHOICE = ROCK;
+
+let gameIsRunning = false;
+
+const getPlayerChoice = function () {
+  const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS}?`).toUpperCase();
+
+  if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
+    alert(`Invalid choice! We chose ${DEFAULT_USER_CHOICE} for you!`);
+    return DEFAULT_USER_CHOICE;
+  }
+  return selection;
 };
 
-// const person = {
-//   name: "Son MinSeock",
-//   greet: function greet() {
-//     console.log("Hello there!");
-//   },
-// };
-
-// person.greet();
-
-//console.log(typeof startGame);
-//console.dir(start);
-
-// startGameBtn.addEventListener("click", function () {
-//   console.log("Game is Starting...", age);
-// });
-startGameBtn.addEventListener("click", start);
+startGameBtn.addEventListener("click", function () {
+  if (gameIsRunning) {
+    return;
+  }
+  gameIsRunning = true;
+  console.log("Game is Starting...");
+  const playerSelection = getPlayerChoice();
+  console.log(playerSelection);
+});
