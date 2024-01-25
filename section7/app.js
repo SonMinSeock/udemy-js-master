0a8dadf3313c8d87e4e468743e4fd9e0f6c8f05b
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-01-25 15:32:49 +0900

    DOM 요소 스타일링하기

--- section7/app.js
+++ section7/app.js
@@ -1,4 +1,16 @@
-const ul = document.body.firstElementChild.nextElementSibling;
-const firstLiElement = ul.firstElementChild;
+const section = document.querySelector("section");
+const toggleBtn = document.querySelector("button");
 
-console.dir(firstLiElement);
+//section.style.backgroundColor = "green";
+//section.className = "red-bg visible";
+section.classList.add("red-bg", "visible");
+
+toggleBtn.addEventListener("click", () => {
+  //   if (section.className === "red-bg visible") {
+  //     section.classList.toggle = "red-bg invisible";
+  //   } else {
+  //     section.className = "red-bg visible";
+  //   }
+  section.classList.toggle("visible");
+  section.classList.toggle("invisible");
+});
