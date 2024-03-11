a0568956b6189bb67e016c2a3675466bd31daa31
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-03-11 17:24:49 +0900

    사용자 입력 페칭 및 검증

--- section7/dom-pratice/dom-06-demo-project-setup/assets/scripts/app.js
+++ section7/dom-pratice/dom-06-demo-project-setup/assets/scripts/app.js
@@ -1,7 +1,9 @@
 const addMovieModal = document.getElementById("add-modal");
 const startAddMovieButton = document.querySelector("header button");
 const backdrop = document.getElementById("backdrop");
-const cansleAddModalButton = document.querySelector(".modal__actions .btn--passive");
+const cancleAddModalButton = document.querySelector(".modal__actions .btn--passive");
+const confirmAddMovieButton = cancleAddModalButton.nextElementSibling;
+const userInputs = addMovieModal.querySelectorAll("input");
 
 function toggleBackdrop() {
   backdrop.classList.toggle("visible");
@@ -20,6 +22,25 @@ function cancleClickHandler() {
   toggleMovieModal();
 }
 
+function addMovieHandler() {
+  const titleValue = userInputs[0].value;
+  const imageUrlValue = userInputs[1].value;
+  const ratingValue = userInputs[2].value;
+
+  if (
+    titleValue.trim() === "" ||
+    imageUrlValue.trim() === "" ||
+    ratingValue.trim() === "" ||
+    +ratingValue < 1 ||
+    +ratingValue > 5
+  ) {
+    alert("Please enter valid values (rating between 1 and 5).");
+  }
+
+  return;
+}
+
 startAddMovieButton.addEventListener("click", toggleMovieModal);
-cansleAddModalButton.addEventListener("click", cancleClickHandler);
+cancleAddModalButton.addEventListener("click", backdropClickHandler);
 backdrop.addEventListener("click", backdropClickHandler);
+confirmAddMovieButton.addEventListener("click", addMovieHandler);
