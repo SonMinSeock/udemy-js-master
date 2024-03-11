c7aecb0924be53fac9792b444de903bd5c98fad3
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-03-11 17:05:04 +0900

    배경(Backdrop) 제어하기

--- section7/dom-pratice/dom-06-demo-project-setup/assets/scripts/app.js
+++ section7/dom-pratice/dom-06-demo-project-setup/assets/scripts/app.js
@@ -1,8 +1,25 @@
 const addMovieModal = document.getElementById("add-modal");
 const startAddMovieButton = document.querySelector("header button");
+const backdrop = document.getElementById("backdrop");
+const cansleAddModalButton = document.querySelector(".modal__actions .btn--passive");
+
+function toggleBackdrop() {
+  backdrop.classList.toggle("visible");
+}
 
 function toggleMovieModal() {
   addMovieModal.classList.toggle("visible");
+  toggleBackdrop();
+}
+
+function backdropClickHandler() {
+  toggleMovieModal();
+}
+
+function cancleClickHandler() {
+  toggleMovieModal();
 }
 
 startAddMovieButton.addEventListener("click", toggleMovieModal);
+cansleAddModalButton.addEventListener("click", cancleClickHandler);
+backdrop.addEventListener("click", backdropClickHandler);
