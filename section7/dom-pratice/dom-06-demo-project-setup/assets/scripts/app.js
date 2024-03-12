c992a4bc5711454f203a8155955f44072e702139
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-03-12 10:50:30 +0900

    화면에 영화 항목 렌더링하기

--- section7/dom-pratice/dom-06-demo-project-setup/assets/scripts/app.js
+++ section7/dom-pratice/dom-06-demo-project-setup/assets/scripts/app.js
@@ -4,8 +4,34 @@ const backdrop = document.getElementById("backdrop");
 const cancleAddModalButton = document.querySelector(".modal__actions .btn--passive");
 const confirmAddMovieButton = cancleAddModalButton.nextElementSibling;
 const userInputs = addMovieModal.querySelectorAll("input");
+const entryTextSection = document.getElementById("entry-text");
 
 const movies = [];
+
+function renderNewMovieElement(title, imageUrl, rating) {
+  const newMovieElement = document.createElement("li");
+  newMovieElement.classList.add("movie-element");
+  newMovieElement.innerHTML = `
+  <div class="movie-element__image">
+  <img src="${imageUrl}" alt="영화 이미지"/>
+  </div>
+  <div class="movie-element__info">
+  <h2>${title}</h2>
+  <p>${rating}</p>
+  </div>
+  `;
+
+  const movieList = document.getElementById("movie-list");
+  movieList.append(newMovieElement);
+}
+
+function updateUI() {
+  if (movies.length === 0) {
+    entryTextSection.style.display = "block";
+  } else {
+    entryTextSection.style.display = "none";
+  }
+}
 
 function clearMovieInput() {
   userInputs.forEach((userInput) => {
@@ -61,6 +87,8 @@ function addMovieHandler() {
   console.log(movies);
 
   toggleAddMovieModal();
+  updateUI();
+  renderNewMovieElement(newMovie.title, newMovie.image, newMovie.rating);
 }
 
 startAddMovieButton.addEventListener("click", startAddMovieClickHandler);
