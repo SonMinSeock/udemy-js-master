d174af7a6ef613d6b2aad0abab57377a35c5f2d9
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-03-14 18:55:22 +0900

    push(), pop(), unshit(), shift() - 요소 추가 & 제거하기

--- section8/arrays-01-starting-code/app.js
+++ section8/arrays-01-starting-code/app.js
@@ -1,38 +1,55 @@
-const numbers = [1, 2, 3];
+// const numbers = [1, 2, 3];
 
-console.log(numbers);
+// console.log(numbers);
 
-// const moreNumbers = new Array("Hi", "Welcome");
-// const moreNumbers = new Array(5);
-// const moreNumbers = new Array(5, 2);
-// const moreNumbers = Array(5);
-const moreNumbers = Array(5, 2);
-console.log(moreNumbers);
+// // const moreNumbers = new Array("Hi", "Welcome");
+// // const moreNumbers = new Array(5);
+// // const moreNumbers = new Array(5, 2);
+// // const moreNumbers = Array(5);
+// const moreNumbers = Array(5, 2);
+// console.log(moreNumbers);
 
-const yewMoreNumbers = Array.of(1, 4);
-console.log(yewMoreNumbers);
+// const yewMoreNumbers = Array.of(1, 4);
+// console.log(yewMoreNumbers);
 
-// const anotherMoreNumbers = Array.from([1, 3]);
-const anotherMoreNumbers = Array.from("hello");
-console.log(anotherMoreNumbers);
+// // const anotherMoreNumbers = Array.from([1, 3]);
+// const anotherMoreNumbers = Array.from("hello");
+// console.log(anotherMoreNumbers);
 
-const listItems = document.querySelectorAll("li");
-console.log(listItems);
+// const listItems = document.querySelectorAll("li");
+// console.log(listItems);
 
-const arrayListItems = Array.from(listItems);
-console.log(arrayListItems);
+// const arrayListItems = Array.from(listItems);
+// console.log(arrayListItems);
 
-const hobbies = ["Cooking", "Sports"];
-const personalData = [30, "Son", { moreDetail: [] }];
-const analyticsData = [
-  [1, 1.6],
-  [-5.4, 2.1],
-];
+// const hobbies = ["Cooking", "Sports"];
+// const personalData = [30, "Son", { moreDetail: [] }];
+// const analyticsData = [
+//   [1, 1.6],
+//   [-5.4, 2.1],
+// ];
 
-for (const data of analyticsData) {
-  for (const dataPoint of data) {
-    console.log(dataPoint);
-  }
-}
+// for (const data of analyticsData) {
+//   for (const dataPoint of data) {
+//     console.log(dataPoint);
+//   }
+// }
 
-console.log(personalData[1]);
+// console.log(personalData[1]);
+
+const hobbies = ["Sports", "Cooking"];
+
+hobbies.push("Reading");
+hobbies.unshift("Coding");
+
+const poppedValue = hobbies.pop();
+hobbies.shift();
+
+console.log(poppedValue);
+console.log(hobbies);
+
+hobbies[1] = "Coding";
+console.log(hobbies);
+
+hobbies[5] = "Reading";
+console.log(hobbies, hobbies[4]);
