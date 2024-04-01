7fc7b231a3f9fb602a0205b4c63a5a10be2a52b3
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-04-01 19:20:49 +0900

    탐색 - find()와 findindex()

--- section8/arrays-01-starting-code/app.js
+++ section8/arrays-01-starting-code/app.js
@@ -1,71 +1,3 @@
-// const numbers = [1, 2, 3];
-
-// console.log(numbers);
-
-// // const moreNumbers = new Array("Hi", "Welcome");
-// // const moreNumbers = new Array(5);
-// // const moreNumbers = new Array(5, 2);
-// // const moreNumbers = Array(5);
-// const moreNumbers = Array(5, 2);
-// console.log(moreNumbers);
-
-// const yewMoreNumbers = Array.of(1, 4);
-// console.log(yewMoreNumbers);
-
-// // const anotherMoreNumbers = Array.from([1, 3]);
-// const anotherMoreNumbers = Array.from("hello");
-// console.log(anotherMoreNumbers);
-
-// const listItems = document.querySelectorAll("li");
-// console.log(listItems);
-
-// const arrayListItems = Array.from(listItems);
-// console.log(arrayListItems);
-
-// const hobbies = ["Cooking", "Sports"];
-// const personalData = [30, "Son", { moreDetail: [] }];
-// const analyticsData = [
-//   [1, 1.6],
-//   [-5.4, 2.1],
-// ];
-
-// for (const data of analyticsData) {
-//   for (const dataPoint of data) {
-//     console.log(dataPoint);
-//   }
-// }
-
-// console.log(personalData[1]);
-
-// const hobbies = ["Sports", "Cooking"];
-
-// hobbies.push("Reading");
-// hobbies.unshift("Coding");
-
-// const poppedValue = hobbies.pop();
-// hobbies.shift();
-
-// console.log(poppedValue);
-// console.log(hobbies);
-
-// hobbies[1] = "Coding";
-// console.log(hobbies);
-
-// hobbies[5] = "Reading";
-// console.log(hobbies, hobbies[4]);
-
-// hobbies.splice(1, 0, "Good Food", "Skating");
-
-// const removedElement = hobbies.splice(0, 1);
-// console.log(hobbies);
-// console.log(removedElement);
-
-// hobbies.splice(-1, 1);
-// console.log(hobbies);
-
-// hobbies.splice(0);
-// console.log(hobbies);
-
 const testResults = [1, 3.4, 3, 5.2, 32.1, 3, 5.2, -10];
 const storedResults = testResults.concat([12, -3, 14, 23])
 
@@ -73,4 +5,15 @@ console.log(testResults.indexOf(5.2));
 console.log(testResults.lastIndexOf(3));
 
 const personData = [{name: "Son MinSeock"}, {name: "Lane"}];
-console.log(personData.indexOf({name: "Lane"}));
+const person = personData.find((person, idx, persons) => {
+    return person.name === "Lane";
+})
+
+person.name = "Lane!!!";
+console.log(person, personData);
+
+const findPersonIndex = personData.findIndex((person, idx, persons) => {
+    return person.name === "Lane!!!" 
+});
+
+console.log(findPersonIndex)
