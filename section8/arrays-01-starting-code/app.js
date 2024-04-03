bfb9d04cb7866862d460dab59fdee34018410610
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-04-03 18:22:35 +0900

    sort()과 reverse() 메서드

--- section8/arrays-01-starting-code/app.js
+++ section8/arrays-01-starting-code/app.js
@@ -1,4 +1,27 @@
 const prices = [10.0, 5.93, 3.25, 6.99];
+prices.sort((a, b) => {
+  if (a > b) {
+    return 1;
+  } else if (a === b) {
+    return 0;
+  } else if (a < b) {
+    return -1;
+  }
+});
+console.log("Sorting : ", prices);
+
+prices.sort((a, b) => {
+  if (a > b) {
+    return -1;
+  } else if (a === b) {
+    return 0;
+  } else if (a < b) {
+    return 1;
+  }
+});
+
+console.log("Reserving : ", prices);
+
 const tax = 0.19;
 const taxAdjustedPrices = prices.map((price, idx, prices) => {
   const priceObj = {
