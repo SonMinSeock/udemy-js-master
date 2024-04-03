99d7cde85e0f4fdfd80a93cb336ad4b05a9a7449
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-04-03 17:28:02 +0900

    반복문 대안 - forEach() 메서드

--- section8/arrays-01-starting-code/app.js
+++ section8/arrays-01-starting-code/app.js
@@ -1,22 +1,17 @@
-const testResults = [1, 3.4, 3, 5.2, 32.1, 3, 5.2, -10];
-const storedResults = testResults.concat([12, -3, 14, 23])
+const prices = [10.0, 5.93, 3.25, 6.99];
+const tax = 0.19;
+const taxAdjustedPrices = [];
 
-console.log(testResults.includes(3.4), testResults.includes(0.3));
-console.log(testResults.indexOf(5.2));
-console.log(testResults.lastIndexOf(3));
+// for (const price of prices) {
+//   taxAdjustedPrices.push(price * (1 + tax));
+// }
 
-const personData = [{name: "Son MinSeock"}, {name: "Lane"}];
-const person = personData.find((person, idx, persons) => {
-    return person.name === "Lane";
-})
-
-console.log(personData.includes({ name: "Son MinSeock" }))
-
-person.name = "Lane!!!";
-console.log(person, personData);
-
-const findPersonIndex = personData.findIndex((person, idx, persons) => {
-    return person.name === "Lane!!!" 
+prices.forEach((price, idx, prices) => {
+  const priceObj = {
+    index: idx,
+    taxAdjPrice: price * (1 + tax),
+  };
+  taxAdjustedPrices.push(priceObj);
 });
 
-console.log(findPersonIndex)
+console.log(taxAdjustedPrices);
