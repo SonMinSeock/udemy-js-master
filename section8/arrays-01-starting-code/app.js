45ab96c767ab01770d8610b3602c8ffa3750870e
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-04-03 17:37:40 +0900

    map()으로 데이터 변환하기

--- section8/arrays-01-starting-code/app.js
+++ section8/arrays-01-starting-code/app.js
@@ -1,17 +1,11 @@
 const prices = [10.0, 5.93, 3.25, 6.99];
 const tax = 0.19;
-const taxAdjustedPrices = [];
-
-// for (const price of prices) {
-//   taxAdjustedPrices.push(price * (1 + tax));
-// }
-
-prices.forEach((price, idx, prices) => {
+const taxAdjustedPrices = prices.map((price, idx, prices) => {
   const priceObj = {
     index: idx,
     taxAdjPrice: price * (1 + tax),
   };
-  taxAdjustedPrices.push(priceObj);
+  return priceObj;
 });
 
 console.log(taxAdjustedPrices);
