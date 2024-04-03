4a4ba15cd0b3eb19d8510b2d18b66ec61bc57d40
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-04-03 17:15:48 +0900

    includes 메서드

--- section8/arrays-01-starting-code/app.js
+++ section8/arrays-01-starting-code/app.js
@@ -1,6 +1,7 @@
 const testResults = [1, 3.4, 3, 5.2, 32.1, 3, 5.2, -10];
 const storedResults = testResults.concat([12, -3, 14, 23])
 
+console.log(testResults.includes(3.4), testResults.includes(0.3));
 console.log(testResults.indexOf(5.2));
 console.log(testResults.lastIndexOf(3));
 
@@ -8,6 +9,8 @@ const personData = [{name: "Son MinSeock"}, {name: "Lane"}];
 const person = personData.find((person, idx, persons) => {
     return person.name === "Lane";
 })
+
+console.log(personData.includes({ name: "Son MinSeock" }))
 
 person.name = "Lane!!!";
 console.log(person, personData);
