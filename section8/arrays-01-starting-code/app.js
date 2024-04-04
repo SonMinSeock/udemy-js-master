854abcb7eade74ae1b032c74ebf3181dec6b2308
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-04-04 22:21:20 +0900

    배열 구조 분해 이해하기

--- section8/arrays-01-starting-code/app.js
+++ section8/arrays-01-starting-code/app.js
@@ -1,38 +1,4 @@
-const prices = [10.0, 5.93, 3.25, 6.99];
-prices.sort((a, b) => {
-  if (a > b) {
-    return 1;
-  } else if (a === b) {
-    return 0;
-  } else if (a < b) {
-    return -1;
-  }
-});
-console.log("Sorting : ", prices);
+const [firstName, lastName, ...otherInformation] = ["손", "민석", 25, "프론트엔드 취준생"];
 
-prices.sort((a, b) => {
-  if (a > b) {
-    return -1;
-  } else if (a === b) {
-    return 0;
-  } else if (a < b) {
-    return 1;
-  }
-});
-
-// prices.forEach((price) => (sum += price));
-// console.log("Prices 총합 : ", sum);
-
-const sum = prices.reduce((prevValue, curValue, curIdx, prices) => {
-  return prevValue + curValue;
-}, 0);
-
-console.log("Prices 총합 : ", sum);
-
-const data = "new york;10.99;20.3";
-const transformedData = data.split(";");
-console.log(transformedData);
-
-const nameFragments = ["Son", "MinSeock"];
-const name = nameFragments.join(" ");
-console.log(name);
+console.log(`${firstName} ${lastName}`);
+console.log(otherInformation);
