b4cafdfa5d3a20e9edc292501dc74ba34c26a556
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-04-05 17:39:46 +0900

    Maps 작업하기

--- section8/arrays-01-starting-code/setsmaps.js
+++ section8/arrays-01-starting-code/setsmaps.js
@@ -10,3 +10,27 @@ console.log(ids);
 if (ids.has(4)) {
   ids.delete(4);
 }
+
+const person1 = { name: "Son" };
+const person2 = { name: "Jane" };
+
+const personData = new Map([[person1, [{ date: "Yesterday", price: 10 }]]]);
+console.log(personData.get(person1));
+
+personData.set(person2, [{ date: "Today", price: 15 }]);
+console.log(personData);
+console.log(personData.get(person2));
+
+for (const [key, value] of personData.entries()) {
+  console.log(key, value);
+}
+
+for (const key of personData.keys()) {
+  console.log(key);
+}
+
+for (const value of personData.values()) {
+  console.log(value);
+}
+
+console.log(personData.size);
