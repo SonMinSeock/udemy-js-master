850232a5d73ed7e8a637a278676ff559acee1fcc
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-04-07 15:28:08 +0900

    객체 복습

---
+++ section9/objects-01-starting-project/assets/scripts/objects.js
@@ -0,0 +1,13 @@
+const person = {
+  name: "손민석",
+  age: 25,
+  hobbies: ["게임", "영화"],
+  //   greet: function () {
+  //     alert("안녕하세요~");
+  //   },
+  greet() {
+    alert("안녕하세요~");
+  },
+};
+
+person.greet();
