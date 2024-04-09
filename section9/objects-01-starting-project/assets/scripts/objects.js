8e7aa8edef31acc32d562fc8313ac7280d3b42b3
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-04-09 15:45:01 +0900

    프로퍼티 유형 & 프로퍼티 순서 & 프로퍼티 동적 접근 및 할당

--- section9/objects-01-starting-project/assets/scripts/objects.js
+++ section9/objects-01-starting-project/assets/scripts/objects.js
@@ -1,22 +1,30 @@
 const movieList = document.getElementById("movie-list");
+const userChosenKeyName = "level";
 const person = {
-  "first-name": "손",
+  "first name": "손",
   age: 25,
   hobbies: ["게임", "영화"],
+  [userChosenKeyName]: 1, // level : 1 객체 프로퍼티를 동적으로 만듬.
   //   greet: function () {
   //     alert("안녕하세요~");
   //   },
   greet() {
     alert("안녕하세요~");
   },
+  1.5: "hello",
 };
+
+console.log(person[1.5]);
+console.log(person);
 
 person.isAdmin = true;
 delete person.age;
 // person.age = undefined;
 // person.age = null;
 person.isAdmin = false;
-console.log(person["first-name"]);
+
+const keyName = "first name";
+console.log(person[keyName]);
 
 movieList.style["background-color"] = "red";
 movieList.style.display = "block";
